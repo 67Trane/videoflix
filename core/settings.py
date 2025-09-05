e2c12a883ceba061defdf2f3c8403da5551aefa6
67Trane <144054852+67Trane@users.noreply.github.com>
2025-09-05 14:26:34 +0200

    fixed Email Port

--- core/settings.py
+++ core/settings.py
@@ -203,11 +203,11 @@ REST_FRAMEWORK = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST= os.getenv("EMAIL_HOST")
-EMAIL_PORT=587
+EMAIL_PORT= int(os.getenv("EMAIL_PORT"))
 EMAIL_HOST_USER=os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD=os.getenv("EMAIL_HOST_PASSWORD")
-EMAIL_USE_TLS=True
-EMAIL_USE_SSL=False
+EMAIL_USE_TLS=bool(os.getenv("EMAIL_USE_TLS"))
+EMAIL_USE_SSL=bool(os.getenv("EMAIL_USE_SSL"))
 
 
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "no-reply@videoflix.local")
